fea46ed8c27d107b0a82aabd1e80e32e2817373e
Alok Ranjan <106423643+aLok-1105@users.noreply.github.com>
2026-05-17 19:08:14 +0530

    Expand skills list for Java Spring certification

--- src/components/CertificationsSection.tsx
+++ src/components/CertificationsSection.tsx
@@ -20,7 +20,7 @@ const certifications: CertificationItem[] = [
     issuer: "Udemy",
     issueDate: "May 2026",
     credentialId: "UC-ba16ee37-0eee-4838-9da7-d3045c6722dd",
-    skills: ["Spring AI", "Hibernate", "JAVA", "Spring Security", "Microservices"],
+    skills: ["JAVA", "Spring", "Spring Boot", "Spring AI", "Hibernate", "Spring Security", "Microservices"],
     credentialUrl: "https://www.udemy.com/certificate/UC-ba16ee37-0eee-4838-9da7-d3045c6722dd/",
     image: "https://udemy-certificate.s3.amazonaws.com/image/UC-ba16ee37-0eee-4838-9da7-d3045c6722dd.jpg",
   },
